8606be8939a79d4945ad3fb9e9c9dafb9e2e3b0b
Dominic Wenger <dominic.wenger@gmail.com>
2015-07-11 18:57:02 +0200

    fixes

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dpkg-deb -bZgzip projects/Flatastic debs
 dpkg-deb -bZgzip projects/Sharingan debs
-dpkg-deb -bZgzip projects/84MusicIconFix
+dpkg-deb -bZgzip projects/84MusicIconFix debs
