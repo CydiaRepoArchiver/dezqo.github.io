3de544138c7f5c5c1502c6a2a1022fcd8161630d
Dominic Wenger <dominic.wenger@gmail.com>
2015-07-13 03:00:05 +0200

    added themes

--- repo/packages.sh
+++ repo/packages.sh
@@ -3,3 +3,4 @@ dpkg-deb -bZgzip projects/Flatastic debs
 dpkg-deb -bZgzip projects/Sharingan debs
 dpkg-deb -bZgzip projects/84MusicIconFix debs
 dpkg-deb -bZgzip projects/Dragonball debs
+dpkg-deb -bZgzip projects/onepiece debs
