5497c5fe5409c415f9e3b65f54762651bb583aad
Dominic Wenger <dominic.wenger@gmail.com>
2015-08-08 20:35:52 +0200

    added stuff

--- repo/packages.sh
+++ repo/packages.sh
@@ -4,3 +4,4 @@ dpkg-deb -bZgzip projects/Sharingan debs
 dpkg-deb -bZgzip projects/84MusicIconFix debs
 dpkg-deb -bZgzip projects/Dragonball debs
 dpkg-deb -bZgzip projects/onepiece debs
+dpkg-deb -bZgzip projects/RequestedRespringLogo debs
