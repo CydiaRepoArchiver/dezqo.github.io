728ef2e5d0f6f14542a6f1b66a23d9a805d69cf9
dominiw <Dominic.wenger@gmail.com>
2016-08-22 14:06:13 +0200

    Removing scripts, changing info

--- repo/packages.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dpkg-deb -bZgzip projects/Flatastic debs
-dpkg-deb -bZgzip projects/Sharingan debs
-dpkg-deb -bZgzip projects/84MusicIconFix debs
-dpkg-deb -bZgzip projects/Dragonball debs
-dpkg-deb -bZgzip projects/onepiece debs
-dpkg-deb -bZgzip projects/RequestedRespringLogo debs
